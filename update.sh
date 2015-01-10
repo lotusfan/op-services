04ba2cebff80d6583b8f9dc78292376632aac3fe
stirp <heroicstirp@163.com>
2015-01-10 20:11:20 +0800

    merge

--- update.sh
+++ update.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+pwd=$PWD # remain where exec sh
+workspace=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P) # here is the dir of project
+cd $workspace
 
+branch=$(git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3) # it's branch name of current git branch
 git checkout develop
+
 git pull
-git merge srpdev
+git merge $branch	# merge developed
 git push
-git checkout srpdev
-git merge develop
+git checkout $branch	# enter original branch
+
+git merge develop	# merge develop branch
+
+cd $pwd			# goto where u exec this sh
